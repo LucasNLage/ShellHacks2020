37a9ff066ccb9c9d8c11dfac2d1b908843924683
Amanda Nisenbaum <amanda.nisenbaum@knights.ucf.edu>
2020-09-26 18:30:32 -0300

    added link to create event

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/homepage/homepage.js'
 import Button from './components/buttons/buttons.js'
+import AdminLink from './components/links/adminLink.js'
+import 'fontsource-roboto';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.root}>
       <header className="App-header">
-      <Button title='Volunteer'/>
-      <p>or</p>
-      <Button title='Coordinator'/>
-     
+        <h1>Welcome to TaskMe</h1>        
+        <Button title='Volunteer'/>
+        <p>or</p>
+        <Button title='Coordinator'/>
+        <AdminLink/>
       </header>
+     
     </div>
   );
 }
