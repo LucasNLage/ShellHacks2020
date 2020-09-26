60078407811e64ada2388aae184979a485b53061
Amanda Nisenbaum <amanda.nisenbaum@knights.ucf.edu>
2020-09-26 15:17:03 -0300

    Moved things around

---
+++ src/components/buttons/buttons.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+function renderComponent(props) {
+    const name = props.title;
+    if (name === 'Volunteer') {
+      return (
+        <Button variant="contained" color="primary">
+            {name}
+        </Button>
+      );
+    } else {
+      return(
+        <Button variant="contained" color="secondary">
+             {name}
+         </Button>
+      );
+    }
+  }
+
+export default function OutlinedButtons(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {renderComponent(props)}
+    </div>
+  );
+//   return (
+    
+//       <Button variant="outlined" color="primary">
+//         {props.title}
+//       </Button>
+//       <Button variant="outlined" color="secondary">
+//         Secondary
+//       </Button>
+//   );
+}
---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//TESTING HOME PAGE
+export default function HomePage() {
+    return (
+        <div>
+            <button>Volunteer</button>
+            <p>or</p>
+            <button>Coordinator</button>
+        </div>
+    );
+}
